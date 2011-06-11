346621d97ec18b8b2c62fcafb27d25ca742b30c2
eliac8@gmail.com <eliac8@gmail.com@8ce5ba0b-3feb-16b7-cc19-72bb162b48c5>
2011-06-11 11:39:52 +0000

    Videostore example Doesn't compile/run at its current state - but good for the article.

--- trunk/ink.example/src/main/java/org/ink/language/example/videostore/RestrictedMovieImpl.java
+++ trunk/ink.example/src/main/java/org/ink/language/example/videostore/RestrictedMovieImpl.java
@@ -3,7 +3,7 @@ package org.ink.language.example.videostore;
 public class RestrictedMovieImpl<S extends RestrictedMovieState> extends MovieImpl<S> implements RestrictedMovie {
 
 	@Override
-	public int getMinimumAge() {
-		return getState().getMinimumAge();
+	public boolean canRent(CustomerState customer) {
+		return getState().getMinimumAge() <= customer.getAge();
 	}
 }
--- trunk/ink.example/src/main/java/org/ink/language/example/videostore/RestrictedVideotapeImpl.java
+++ trunk/ink.example/src/main/java/org/ink/language/example/videostore/RestrictedVideotapeImpl.java
@@ -1,12 +1,10 @@
 package org.ink.language.example.videostore;
 
-public class RestrictedVideotapeImpl<S extends RestrictedVideotapeState> extends VideotapeImpl<S> implements RestrictedVideotape {
+public class RestrictedVideotapeImpl<S extends RestrictedVideotapeState> extends VideotapeImpl<S> implements Videotape {
 
 	@Override
 	public boolean canRent(CustomerState customer) {
 		RestrictedMovie meta = getMeta();
-		int minimumAge = meta.getMinimumAge();
-		int customerAge = customer.getAge();
-		return super.canRent(customer) && customerAge >= minimumAge;
+		return super.canRent(customer) && meta.canRent(customer);
 	}
 }

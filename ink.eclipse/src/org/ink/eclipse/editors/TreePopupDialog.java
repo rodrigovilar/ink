84580a84d413d57a415744d1660acbd27485923f
eliac8 <eliac8@gmail.com>
2011-09-23 13:47:57 +0000

    Ctrl+T - instance-of hierarchy + dialog class refactor

---
+++ ink.eclipse/src/org/ink/eclipse/editors/TreePopupDialog.java
@@ -0,0 +1,176 @@
+package org.ink.eclipse.editors;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.jface.dialogs.PopupDialog;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.viewers.AbstractTreeViewer;
+import org.eclipse.jface.viewers.DecoratingStyledCellLabelProvider;
+import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.StyledString;
+import org.eclipse.jface.viewers.TreeNode;
+import org.eclipse.jface.viewers.TreeNodeContentProvider;
+import org.eclipse.jface.viewers.TreePath;
+import org.eclipse.jface.viewers.TreeSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.MouseAdapter;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+import org.ink.eclipse.InkPlugin;
+import org.ink.eclipse.utils.SimpleTreeNode;
+
+public class TreePopupDialog<T> extends PopupDialog {
+
+	public static interface JumpHandler<T> {
+		public void doJump(T object);
+	}
+
+	protected SimpleTreeNode<T> rootNode;
+	protected SimpleTreeNode<T> selectedNode;
+	protected final JumpHandler<T> jumpHandler;
+	protected KeyAdapter keyAdapter;
+	protected IStyledLabelProvider labelProvider;
+	protected Tree tree;
+	protected TreeViewer treeViewer;
+
+	public TreePopupDialog(SimpleTreeNode<T> rootNode, SimpleTreeNode<T> selectedNode, JumpHandler<T> jumpHandler, String title, Shell shell) {
+		super(shell, SWT.RESIZE, true, true, false, true, true, null, null);
+		this.rootNode = rootNode;
+		this.selectedNode = selectedNode;
+		this.jumpHandler = jumpHandler;
+		this.keyAdapter = new TreePopupDialogKeyAdapter();
+		this.labelProvider = new TreePopupDialogLabelProvider<T>();
+		setTitleText(title);
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		tree = new Tree(parent, SWT.SINGLE | ((SWT.V_SCROLL | SWT.H_SCROLL) & ~SWT.MULTI));
+		GridData gd = new GridData(GridData.FILL_BOTH);
+		gd.heightHint = tree.getItemHeight() * 12;
+		tree.setLayoutData(gd);
+
+		treeViewer = new TreeViewer(tree);
+		treeViewer.setAutoExpandLevel(AbstractTreeViewer.ALL_LEVELS);
+		treeViewer.setLabelProvider(new DecoratingStyledCellLabelProvider(labelProvider, null, null));
+		treeViewer.setContentProvider(new TreeNodeContentProvider());
+		doSetInput();
+
+		tree.addKeyListener(keyAdapter);
+		tree.addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mouseDoubleClick(MouseEvent e) {
+				callJumpHandler();
+			}
+		});
+		return treeViewer.getControl();
+	}
+
+	@SuppressWarnings("unchecked")
+	private void callJumpHandler() {
+		TreeItem[] selection = tree.getSelection();
+		if (selection != null && selection.length > 0 && jumpHandler != null) {
+			TreeNode selectedNode = (TreeNode) selection[0].getData();
+			jumpHandler.doJump((T) selectedNode.getValue());
+		}
+		close();
+	}
+
+	protected void doSetInput() {
+		TreeNode selectedEclipseNode = new TreeNode(selectedNode.getValue());
+		treeViewer.setInput(new TreeNode[] { toEclipseTreeNode(rootNode, selectedEclipseNode) });
+		TreeNode tempNodeForSelection = selectedEclipseNode;
+		List<TreeNode> pathToOriginalObjectNode = new ArrayList<TreeNode>();
+		while (tempNodeForSelection != null) {
+			pathToOriginalObjectNode.add(0, tempNodeForSelection);
+			tempNodeForSelection = tempNodeForSelection.getParent();
+		}
+		treeViewer.setSelection(new TreeSelection(new TreePath(pathToOriginalObjectNode.toArray())));
+	}
+
+	private TreeNode toEclipseTreeNode(SimpleTreeNode<T> node, TreeNode selectedEclipseNode) {
+		TreeNode result = node == selectedNode ? selectedEclipseNode : new TreeNode(node.getValue());
+		List<SimpleTreeNode<T>> children = node.getChildren();
+		TreeNode[] eclipseChildren = new TreeNode[children.size()];
+		int currentChild = 0;
+		for (SimpleTreeNode<T> child : children) {
+			eclipseChildren[currentChild] = toEclipseTreeNode(child, selectedEclipseNode);
+			eclipseChildren[currentChild].setParent(result);
+			currentChild++;
+		}
+		result.setChildren(eclipseChildren);
+		return result;
+	}
+
+	protected static class TreePopupDialogLabelProvider<T> extends LabelProvider implements IStyledLabelProvider {
+
+		private final Map<String, Image> imagesMap;
+
+		public TreePopupDialogLabelProvider() {
+			imagesMap = new HashMap<String, Image>(4);
+		}
+
+		@SuppressWarnings("unchecked")
+		@Override
+		public final StyledString getStyledText(Object element) {
+			return doGetStyledText((T) ((TreeNode) element).getValue());
+		}
+
+		@Override
+		public final Image getImage(Object element) {
+			Image result = null;
+			@SuppressWarnings("unchecked")
+			String filename = doGetImageFilename((T) ((TreeNode) element).getValue());
+			if (filename != null) {
+				result = imagesMap.get(filename);
+				if (result == null) {
+					result = ImageDescriptor.createFromFile(InkPlugin.class, "/resources/icons/" + filename).createImage(true);
+					imagesMap.put(filename, result);
+				}
+			}
+			return result;
+		}
+
+		protected StyledString doGetStyledText(T element) {
+			return new StyledString(element.toString());
+		}
+
+		protected String doGetImageFilename(T element) {
+			return null;
+		}
+
+		@Override
+		public void dispose() {
+			for (Image image : imagesMap.values()) {
+				image.dispose();
+			}
+			super.dispose();
+		}
+	}
+
+	protected class TreePopupDialogKeyAdapter extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			if (e.character == java.awt.event.KeyEvent.VK_ESCAPE) {
+				close();
+			} else if (e.character == java.awt.event.KeyEvent.VK_ENTER) {
+				callJumpHandler();
+			}
+		}
+	}
+}
--- ink.eclipse/src/org/ink/eclipse/editors/operations/QuickHierarchyOperation.java
+++ ink.eclipse/src/org/ink/eclipse/editors/operations/QuickHierarchyOperation.java
@@ -1,40 +1,17 @@
 package org.ink.eclipse.editors.operations;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.jface.bindings.TriggerSequence;
 import org.eclipse.jface.bindings.keys.KeySequence;
 import org.eclipse.jface.bindings.keys.SWTKeySupport;
-import org.eclipse.jface.dialogs.PopupDialog;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.viewers.AbstractTreeViewer;
-import org.eclipse.jface.viewers.DecoratingStyledCellLabelProvider;
-import org.eclipse.jface.viewers.DelegatingStyledCellLabelProvider.IStyledLabelProvider;
-import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.StyledString;
-import org.eclipse.jface.viewers.TreeNode;
-import org.eclipse.jface.viewers.TreeNodeContentProvider;
-import org.eclipse.jface.viewers.TreePath;
-import org.eclipse.jface.viewers.TreeSelection;
-import org.eclipse.jface.viewers.TreeViewer;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
-import org.eclipse.swt.events.MouseAdapter;
-import org.eclipse.swt.events.MouseEvent;
-import org.eclipse.swt.graphics.Image;
-import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.keys.IBindingService;
 import org.ink.core.vm.lang.InkObject;
@@ -43,8 +20,11 @@ import org.ink.core.vm.mirror.Mirror;
 import org.ink.core.vm.modelinfo.ModelInfoFactory;
 import org.ink.core.vm.modelinfo.ModelInfoRepository;
 import org.ink.core.vm.modelinfo.relations.ExtendsRelation;
+import org.ink.core.vm.modelinfo.relations.IsInstanceOfRelation;
+import org.ink.core.vm.modelinfo.relations.ModelRelation;
 import org.ink.core.vm.types.ObjectTypeMarker;
-import org.ink.eclipse.InkPlugin;
+import org.ink.eclipse.editors.TreePopupDialog;
+import org.ink.eclipse.editors.TreePopupDialog.JumpHandler;
 import org.ink.eclipse.utils.EclipseUtils;
 import org.ink.eclipse.utils.SimpleTreeNode;
 
@@ -52,201 +32,189 @@ public class QuickHierarchyOperation extends InkEditorOperation {
 
 	public static final String INK_ECLIPSE_QUICK_HIERARCHY = "ink.eclipse.quickHierarchy";
 
+	private InkObject originalObject;
+
 	@Override
 	protected boolean execute(InkObject o, Shell shell, IDocument doc, IFile file) throws Exception {
 		if (o != null) {
-			SimpleTreeNode<Mirror> originalObjectNode = createOriginalObjectNode(o);
-			SimpleTreeNode<Mirror> hierarchyTreeRoot = createHierarchyTree(o, originalObjectNode);
-			displayTree(hierarchyTreeRoot, originalObjectNode, shell);
+			this.originalObject = o;
+			displayTree(shell);
 		}
 		return false;
 	}
 
-	private SimpleTreeNode<Mirror> createOriginalObjectNode(InkObject inkObject) {
-		return new SimpleTreeNode<Mirror>(inkObject.<Mirror> reflect());
-	}
-
-	private SimpleTreeNode<Mirror> createHierarchyTree(InkObject inkObject, SimpleTreeNode<Mirror> originalObjectNode) {
+	private SimpleTreeNode<Mirror>[] doBuildHierarchy(HierarchyType type) {
+		@SuppressWarnings("unchecked")
+		SimpleTreeNode<Mirror>[] result = new SimpleTreeNode[2];
+		SimpleTreeNode<Mirror> originalObjectNode = new SimpleTreeNode<Mirror>(originalObject.<Mirror> reflect());
 		SimpleTreeNode<Mirror> rootNode = originalObjectNode;
-		Mirror superMirror = originalObjectNode.getValue().getSuper();
-		while (superMirror != null) {
-			SimpleTreeNode<Mirror> currentNode = new SimpleTreeNode<Mirror>(superMirror);
+		Mirror nextMirror = type.getNextMirror(originalObjectNode.getValue());
+		while (nextMirror != null) {
+			SimpleTreeNode<Mirror> currentNode = new SimpleTreeNode<Mirror>(nextMirror);
 			currentNode.addChildNode(rootNode);
 			rootNode = currentNode;
-			superMirror = superMirror.getSuper();
+			Mirror nextMirrorCandidate = type.getNextMirror(nextMirror);
+			nextMirror = nextMirror == nextMirrorCandidate ? null : nextMirrorCandidate;
 		}
 
 		ModelInfoRepository modelInfoRepository = ModelInfoFactory.getInstance();
-		addDescendents(inkObject, originalObjectNode, modelInfoRepository);
-		return rootNode;
+		addDescendents(originalObject, originalObjectNode, modelInfoRepository, type.getRelation());
+		result[0] = rootNode;
+		result[1] = originalObjectNode;
+		return result;
 	}
 
-	protected void addDescendents(InkObject inkObject, SimpleTreeNode<Mirror> objectNode, ModelInfoRepository modelInfoRepository) {
-		Collection<InkObject> descendents = modelInfoRepository.findReferrers(inkObject, ExtendsRelation.getInstance(), false);
+	protected void addDescendents(InkObject inkObject, SimpleTreeNode<Mirror> objectNode, ModelInfoRepository modelInfoRepository, ModelRelation relation) {
+		Collection<InkObject> descendents = modelInfoRepository.findReferrers(inkObject, relation, false);
 		for (InkObject descendent : descendents) {
 			SimpleTreeNode<Mirror> descendentNode = new SimpleTreeNode<Mirror>(descendent.reflect());
 			objectNode.addChildNode(descendentNode);
-			addDescendents(descendent, descendentNode, modelInfoRepository);
+			addDescendents(descendent, descendentNode, modelInfoRepository, relation);
 		}
 	}
 
-	private void displayTree(SimpleTreeNode<Mirror> hierarchyTreeRoot, SimpleTreeNode<Mirror> originalObjectNode, Shell shell) {
-		new QuickHierarchyDialog(hierarchyTreeRoot, originalObjectNode, new JumpHandler<Mirror>() {
+	private void displayTree(Shell shell) {
+		new QuickHierarchyDialog(new HierarchyBuilder() {
+
+			@Override
+			public SimpleTreeNode<Mirror>[] buildHierarchy(HierarchyType type) {
+				return doBuildHierarchy(type);
+			}
+		}, new JumpHandler<Mirror>() {
 
 			@Override
 			public void doJump(Mirror object) {
 				EclipseUtils.openEditor(object);
 			}
-		}, "Type hierarchy for '" + originalObjectNode.getValue().getId() + "'", shell).open();
+		}, shell).open();
 	}
 
-	private static interface JumpHandler<T> {
-		public void doJump(T object);
+	private static interface HierarchyBuilder {
+		SimpleTreeNode<Mirror>[] buildHierarchy(HierarchyType type);
 	}
 
-	private static class QuickHierarchyDialog<T> extends PopupDialog {
+	private static enum HierarchyType {
 
-		private final SimpleTreeNode<T> rootNode;
-		private final SimpleTreeNode<T> originalObjectNode;
-		private final JumpHandler<T> jumpHandler;
-		private final TriggerSequence keyBinding;
+		EXTENDS {
 
-		public QuickHierarchyDialog(SimpleTreeNode<T> rootNode, SimpleTreeNode<T> originalObjectNode, JumpHandler<T> jumpHandler, String title, Shell shell) {
-			super(shell, SWT.RESIZE, true, true, false, true, true, null, null);
-			this.rootNode = rootNode;
-			this.originalObjectNode = originalObjectNode;
-			this.jumpHandler = jumpHandler;
+			@Override
+			public Mirror getNextMirror(Mirror value) {
+				return value.getSuper();
+			}
+
+			@Override
+			public ModelRelation getRelation() {
+				return ExtendsRelation.getInstance();
+			}
+
+			@Override
+			public String getLabel() {
+				return "Type";
+			}
+		},
+		INSTANCE_OF {
+			@Override
+			public Mirror getNextMirror(Mirror value) {
+				return value.getClassMirror();
+			}
+
+			@Override
+			public ModelRelation getRelation() {
+				return IsInstanceOfRelation.getInstance();
+			}
+
+			@Override
+			public String getLabel() {
+				return "Instance-of";
+			}
+		};
+
+		protected abstract Mirror getNextMirror(Mirror value);
+
+		protected abstract ModelRelation getRelation();
+
+		protected abstract String getLabel();
+
+		protected HierarchyType getNext() {
+			HierarchyType[] allValues = values();
+			return allValues[(ordinal() + 1) % allValues.length];
+		}
+	}
+
+	private static class QuickHierarchyDialog extends TreePopupDialog<Mirror> {
+
+		protected final TriggerSequence keyBinding;
+
+		private HierarchyType currentHierarchyType;
+		private final HierarchyBuilder hierarchyBuilder;
+
+		public QuickHierarchyDialog(HierarchyBuilder hierarchyBuilder, JumpHandler<Mirror> jumpHandler, Shell shell) {
+			super(null, null, jumpHandler, null, shell);
+			this.hierarchyBuilder = hierarchyBuilder;
 			TriggerSequence[] activeBindings = ((IBindingService) PlatformUI.getWorkbench().getService(IBindingService.class)).getActiveBindingsFor(INK_ECLIPSE_QUICK_HIERARCHY);
-			if (activeBindings != null && activeBindings.length > 0) {
-				this.keyBinding = activeBindings[0];
-				setInfoText("Press '" + keyBinding.format() + "' to see the instance-of hierarchy");
-			} else {
-				this.keyBinding = null;
-			}
-			setTitleText(title);
+			this.keyBinding = activeBindings != null && activeBindings.length > 0 ? activeBindings[0] : null;
+			this.keyAdapter = new QuickHierarchyKeyAdapter();
+			this.labelProvider = new QuickHierarchyLabelProvider();
+			applyHierarchy(HierarchyType.EXTENDS);
 		}
 
-		@Override
-		protected Control createDialogArea(Composite parent) {
-			TreeViewer fTreeViewer = createTreeViewer(parent, SWT.V_SCROLL | SWT.H_SCROLL);
-			final Tree tree = fTreeViewer.getTree();
-			tree.addKeyListener(new KeyAdapter() {
+		private void applyHierarchy(HierarchyType type) {
+			currentHierarchyType = type;
+			SimpleTreeNode<Mirror>[] nodes = hierarchyBuilder.buildHierarchy(type);
+			this.rootNode = nodes[0];
+			this.selectedNode = nodes[1];
+			setTitleText(type.getLabel() + " hierarchy for '" + selectedNode.getValue().getId() + "'");
+			if (keyBinding != null) {
+				setInfoText("Press '" + keyBinding.format() + "' to see the " + type.getNext().getLabel().toLowerCase() + " hierarchy");
+			}
+		}
 
-				@Override
-				public void keyPressed(KeyEvent e) {
-					int accelerator = SWTKeySupport.convertEventToUnmodifiedAccelerator(e);
-					KeySequence keySequence = KeySequence.getInstance(SWTKeySupport.convertAcceleratorToKeyStroke(accelerator));
-					if (keyBinding != null && keyBinding.equals(keySequence)) {
-						e.doit = false;
-						//toggleHierarchy();
-					} else if (e.character == 0x1B) {
-						close();
-					} else if (e.character == 0xD) {
-						callJumpHandler(tree);
-					}
+		private void toggleHierarchy() {
+			applyHierarchy(currentHierarchyType.getNext());
+			doSetInput();
+		}
+
+		protected class QuickHierarchyKeyAdapter extends TreePopupDialogKeyAdapter {
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int accelerator = SWTKeySupport.convertEventToUnmodifiedAccelerator(e);
+				KeySequence keySequence = KeySequence.getInstance(SWTKeySupport.convertAcceleratorToKeyStroke(accelerator));
+				if (keyBinding != null && keyBinding.equals(keySequence)) {
+					e.doit = false;
+					toggleHierarchy();
+				} else {
+					super.keyPressed(e);
 				}
-			});
-			tree.addMouseListener(new MouseAdapter() {
-
-				@Override
-				public void mouseDoubleClick(MouseEvent e) {
-					callJumpHandler(tree);
-				}
-			});
-			return fTreeViewer.getControl();
-		}
-
-		private void callJumpHandler(Tree tree) {
-			TreeItem[] selection = tree.getSelection();
-			if (selection != null && selection.length > 0 && jumpHandler != null) {
-				TreeNode selectedNode = (TreeNode) selection[0].getData();
-				jumpHandler.doJump((T) selectedNode.getValue());
 			}
-			close();
 		}
 
-		protected TreeViewer createTreeViewer(Composite parent, int style) {
-			Tree tree = new Tree(parent, SWT.SINGLE | (style & ~SWT.MULTI));
-			GridData gd = new GridData(GridData.FILL_BOTH);
-			gd.heightHint = tree.getItemHeight() * 12;
-			tree.setLayoutData(gd);
+		private static class QuickHierarchyLabelProvider extends TreePopupDialogLabelProvider<Mirror> {
 
-			TreeViewer treeViewer = new TreeViewer(tree);
-			treeViewer.setAutoExpandLevel(AbstractTreeViewer.ALL_LEVELS);
-			treeViewer.setLabelProvider(new DecoratingStyledCellLabelProvider(new InkLabelProvider(), null, null));
-			treeViewer.setContentProvider(new TreeNodeContentProvider());
-			TreeNode originalObjectEclipseNode = new TreeNode(originalObjectNode.getValue());
-			treeViewer.setInput(new TreeNode[] { toEclipseTreeNode(rootNode, originalObjectEclipseNode) });
-			TreeNode tempNodeForSelection = originalObjectEclipseNode;
-			List<TreeNode> pathToOriginalObjectNode = new ArrayList<TreeNode>();
-			while (tempNodeForSelection != null) {
-				pathToOriginalObjectNode.add(0, tempNodeForSelection);
-				tempNodeForSelection = tempNodeForSelection.getParent();
+			private static final String STRUCT = "Struct";
+			private final static Map<String, String> filenamesMap;
+			static {
+				filenamesMap = new HashMap<String, String>(5);
+				filenamesMap.put(ObjectTypeMarker.Metaclass.toString(), "metaclass.png");
+				filenamesMap.put(ObjectTypeMarker.Class.toString(), "class.png");
+				filenamesMap.put(ObjectTypeMarker.Object.toString(), "object.png");
+				filenamesMap.put(ObjectTypeMarker.Enumeration.toString(), "enum.png");
+				filenamesMap.put(STRUCT, "struct.png");
 			}
-			treeViewer.setSelection(new TreeSelection(new TreePath(pathToOriginalObjectNode.toArray())));
-			return treeViewer;
-		}
 
-		private TreeNode toEclipseTreeNode(SimpleTreeNode<T> node, TreeNode originalObjectEclipseNode) {
-			TreeNode result = node == originalObjectNode ? originalObjectEclipseNode : new TreeNode(node.getValue());
-			List<SimpleTreeNode<T>> children = node.getChildren();
-			TreeNode[] eclipseChildren = new TreeNode[children.size()];
-			int currentChild = 0;
-			for (SimpleTreeNode<T> child : children) {
-				eclipseChildren[currentChild] = toEclipseTreeNode(child, originalObjectEclipseNode);
-				eclipseChildren[currentChild].setParent(result);
-				currentChild++;
+			@Override
+			protected StyledString doGetStyledText(Mirror element) {
+				StyledString result = new StyledString(element.getShortId());
+				result.append(" - " + element.getNamespace(), StyledString.QUALIFIER_STYLER);
+				return result;
 			}
-			result.setChildren(eclipseChildren);
-			return result;
-		}
-	}
 
-	private static class InkLabelProvider extends LabelProvider implements IStyledLabelProvider {
-
-		private static final String STRUCT = "Struct";
-		private final Map<String, Image> imagesMap;
-		private final static Map<String, String> filenamesMap;
-		static {
-			filenamesMap = new HashMap<String, String>(5);
-			filenamesMap.put(ObjectTypeMarker.Metaclass.toString(), "metaclass.png");
-			filenamesMap.put(ObjectTypeMarker.Class.toString(), "class.png");
-			filenamesMap.put(ObjectTypeMarker.Object.toString(), "object.png");
-			filenamesMap.put(ObjectTypeMarker.Enumeration.toString(), "enum.png");
-			filenamesMap.put(STRUCT, "struct.png");
-		}
-
-		public InkLabelProvider() {
-			imagesMap = new HashMap<String, Image>(4);
-		}
-
-		@Override
-		public StyledString getStyledText(Object element) {
-			Mirror mirror = (Mirror) ((TreeNode) element).getValue();
-			StyledString result = new StyledString(mirror.getShortId());
-			result.append(" - " + mirror.getNamespace(), StyledString.QUALIFIER_STYLER);
-			return result;
-		}
-
-		@Override
-		public Image getImage(Object element) {
-			Mirror mirror = (Mirror) ((TreeNode) element).getValue();
-			ObjectTypeMarker objectType = mirror.getObjectTypeMarker();
-			String imageKey = (objectType == ObjectTypeMarker.Class && ((ClassMirror) mirror).isStruct()) ? STRUCT : objectType.toString();
-			Image result = imagesMap.get(imageKey);
-			if (result == null) {
-				result = ImageDescriptor.createFromFile(InkPlugin.class, "/resources/icons/" + filenamesMap.get(imageKey)).createImage(true);
-				imagesMap.put(imageKey, result);
+			@Override
+			protected String doGetImageFilename(Mirror element) {
+				ObjectTypeMarker objectType = element.getObjectTypeMarker();
+				String imageKey = (objectType == ObjectTypeMarker.Class && ((ClassMirror) element).isStruct()) ? STRUCT : objectType.toString();
+				return filenamesMap.get(imageKey);
 			}
-			return result;
-		}
-
-		@Override
-		public void dispose() {
-			for (Image image : imagesMap.values()) {
-				image.dispose();
-			}
-			super.dispose();
 		}
 	}
 }

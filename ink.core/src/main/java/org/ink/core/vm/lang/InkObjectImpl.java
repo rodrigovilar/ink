5656a8d1488c789499f9255efaaf309d36c3c8cb
eliac8 <eliac8@gmail.com>
2012-01-16 16:25:00 +0000

    Added Javadoc.

--- ink.core/src/main/java/org/ink/core/vm/lang/InkObjectImpl.java
+++ ink.core/src/main/java/org/ink/core/vm/lang/InkObjectImpl.java
@@ -3,27 +3,49 @@ package org.ink.core.vm.lang;
 import org.ink.core.vm.constraints.SystemState;
 import org.ink.core.vm.constraints.ValidationContext;
 import org.ink.core.vm.factory.Context;
+import org.ink.core.vm.factory.DslFactory;
 import org.ink.core.vm.mirror.Mirror;
 import org.ink.core.vm.proxy.Proxiable;
 import org.ink.core.vm.traits.Trait;
 
 /**
+ * The the base Ink behavior object. Every behavior object in Ink extends this class (explicitly or implicitly).
+ * 
+ * @see InkObject
+ * @see InkObjectState
+ * 
  * @author Lior Schachter
  */
 public class InkObjectImpl<S extends InkObjectState> implements InkObject {
 	private S state = null;
 	private Context context = null;
 
+	/**
+	 * This method is invoked by the Ink VM in order to inject the behavior object with its state and context objects.
+	 * <br/>
+	 * <b>This method is intended for internal use by the Ink VM only.</b>
+	 */
 	@SuppressWarnings("unchecked")
 	protected void setState(InkObjectState state, Context context) {
 		this.state = (S) state;
 		this.context = context;
 	}
 
+	/**
+	 * Retrieves the state object of this behavior object. The state object contains the values of the properties defined in this object's class.
+	 * The state object is guaranteed to be non-null.
+	 * <br/>
+	 * The behavior object should never expose its state object to external clients. The state should be accessible to its behavior only.
+	 * @return the state object of this behavior object.
+	 */
 	protected S getState() {
 		return state;
 	}
 
+	/**
+	 * The {@link Context} of this behavior object. This is the <code>context</code> trait of the {@link DslFactory} that loaded this object.
+	 * @return the {@link Context} of this behavior object.
+	 */
 	protected Context getContext() {
 		return context;
 	}
@@ -57,11 +79,17 @@ public class InkObjectImpl<S extends InkObjectState> implements InkObject {
 		return false;
 	}
 
+	/**
+	 * Returns a string representation of this object's state in the Ink source format.
+	 */
 	@Override
 	public String toString() {
 		return state.toString();
 	}
 
+	/**
+	 * Returns <code>true</code> if <code>obj</code> is an Ink behavior object whose ID is equal to this object's ID, <code>false</code> otherwise.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -74,6 +102,9 @@ public class InkObjectImpl<S extends InkObjectState> implements InkObject {
 		return false;
 	}
 
+	/**
+	 * Returns <code>Kind.Behavior</code>.
+	 */
 	@Override
 	public Kind getObjectKind() {
 		return Proxiable.Kind.Behavior;

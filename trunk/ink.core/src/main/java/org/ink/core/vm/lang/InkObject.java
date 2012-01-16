085f57500b81b2ca8750902663bf95119c6ab9e8
eliac8@gmail.com <eliac8@gmail.com@8ce5ba0b-3feb-16b7-cc19-72bb162b48c5>
2012-01-16 16:25:00 +0000

    Added Javadoc.

--- trunk/ink.core/src/main/java/org/ink/core/vm/lang/InkObject.java
+++ trunk/ink.core/src/main/java/org/ink/core/vm/lang/InkObject.java
@@ -1,20 +1,69 @@
 package org.ink.core.vm.lang;
 
+import org.ink.core.vm.constraints.InstanceValidator;
+import org.ink.core.vm.constraints.PropertyValueValidator;
 import org.ink.core.vm.constraints.SystemState;
 import org.ink.core.vm.constraints.ValidationContext;
+import org.ink.core.vm.constraints.Validator;
+import org.ink.core.vm.mirror.editor.ObjectEditor;
 import org.ink.core.vm.proxy.Proxiable;
 
 /**
- * @author Lior chachter
+ * The interface of the base Ink behavior object. Every behavior object in Ink implements this interface (explicitly or via inheritance).
+ * 
+ * @see InkObjectImpl
+ * @see InkObjectState
+ * 
+ * @author Lior Schachter
  */
 public interface InkObject extends Proxiable {
 
+	/**
+	 * A hook method, notifying the behavior object that the state object has been created (or modified externally). This method is a rough
+	 * equivalent of a constructor method in Java. However, note that it may be invoked multiple times during the lifecycle
+	 * of a behavior object: when a behavior is first created and injected with its state object, and after the state object has been modified
+	 * (e.g. by an {@link ObjectEditor}).
+	 * <br/>
+	 * <b>This method should contain initialization logic of the behavior object.</b>
+	 */
 	public void afterStateSet();
 
+	/**
+	 * Creates a deep clone of this behavior object's state. The clone operation traverses the state object and creates a clone of every mutable
+	 * object it encounters, such as inner state objects and collections. (Primitive values are immutable and therefore are shallow-copied.)
+	 * <br/>
+	 * Object IDs are not cloned. The cloned state object and all its inner objects have unique IDs. Behavior objects are not created during this
+	 * operation.
+	 * @return a deep clone of this behavior object's state.
+	 */
 	public <T extends InkObjectState> T cloneState();
 
+	/**
+	 * Validates this object by executing the validators from the <code>constraints</code> trait relevant to the required system state.
+	 * The messages reported during validation are collected into the context object.
+	 * <br/>
+	 * <b>Application code shouldn't override this method. Validation logic should be modified by changing the <code>constraints</code> trait.</b>
+	 * @param context a {@link ValidationContext} object into which the validation messages are collected.
+	 * @param systemState denotes whether design-time or run-time validators should be invoked.
+	 * @return <code>true</code> if no validation errors were encountered, <code>false</code> otherwise.
+	 * @see Validator
+	 * @see InstanceValidator#validate(InkObjectState, org.ink.core.vm.mirror.Mirror, ValidationContext, SystemState)
+	 * @see PropertyValueValidator#validate(Property, Object, InkObjectState, ValidationContext, SystemState)
+	 */
 	public boolean validate(ValidationContext context, SystemState systemState);
 
+	/**
+	 * Validates this object by executing the validators from the <code>constraints</code> trait relevant to run-time system state.
+	 * Equivalent to calling <code>validate(context, SystemState.Run_Time)</code>.
+	 * The messages reported during validation are collected into the context object.
+	 * <br/>
+	 * <b>Application code shouldn't override this method. Validation logic should be modified by changing the <code>constraints</code> trait.</b>
+	 * @param context a {@link ValidationContext} object into which the validation messages are collected.
+	 * @return <code>true</code> if no validation errors were encountered, <code>false</code> otherwise.
+	 * @see Validator
+	 * @see InstanceValidator#validate(InkObjectState, org.ink.core.vm.mirror.Mirror, ValidationContext, SystemState)
+	 * @see PropertyValueValidator#validate(Property, Object, InkObjectState, ValidationContext, SystemState)
+	 */
 	public boolean validate(ValidationContext context);
 
 }
--- trunk/ink.core/src/main/java/org/ink/core/vm/proxy/Proxiable.java
+++ trunk/ink.core/src/main/java/org/ink/core/vm/proxy/Proxiable.java
@@ -7,66 +7,80 @@ import org.ink.core.vm.traits.Trait;
 
 /**
  * 
- * Proxiability of a class is enabled by the class implementing the org.ink.core.vm.proxy.Proxiable interface.
+ * Proxiability of a class is enabled by the class implementing <code>Proxiable</code> interface.
+ * <br/>
  * Classes that do not implement this interface will not have their state proxied.
+ * <br/>
  * All subtypes of a proxiable class are themselves proxiable.
- * The Proxiable interface declares a predicate named isProxied, which returns a boolean value.
- * 
+ * <br/>
+ * The <code>Proxiable</code> interface declares a predicate named {@link #isProxied}, which returns a boolean value.
  */
-
 public interface Proxiable {
 
+	/**
+	 * The kind of proxiable object (state or behavior).
+	 */
 	public enum Kind {
 		State, Behavior;
 	}
 
+	/**
+	 * Returns the kind of proxiable object (state or behavior).
+	 * @return the kind of proxiable object (state or behavior).
+	 */
 	public Kind getObjectKind();
 
+	/**
+	 * Returns <code>true</code> if this object is actually a proxy.
+	 * @return <code>true</code> if this object is a proxy, <code>false</code> otherwise.
+	 */
 	public boolean isProxied();
 
 	/**
-	 * Cast as a trait. A trait is a part of an object's personality.
-	 * 
-	 * For example:
-	 * 
+	 * Cast this object as a trait. A trait is a part of an object's personality.
+	 * <br/>
+	 * Example:
 	 * <pre>
-	 * {
-	 * 	&#064;code
-	 * 	public static final byte t_reflection = 0;
-	 * }
-	 * {
-	 * 	&#064;code
-	 * 	Trait trait = this.asTrait(t_reflection);
-	 * }
-	 * {
-	 * 	&#064;code
-	 * 	InkObject object = trait.getTargetBehavior();
-	 * }
+	 *     public static final byte t_reflection = 0;
+	 *     Trait trait = this.asTrait(t_reflection);
+	 * 	   InkObject object = trait.getTargetBehavior();
 	 * </pre>
 	 * 
-	 * @param index
-	 *            an absolute index of the trait
-	 * @return the i-th trait
+	 * @param index an absolute index of the trait.
+	 * @return the <i>i</i>-th trait.
 	 * @see org.ink.core.vm.traits.Trait
 	 * @see org.ink.core.vm.lang.InkObjectImpl#asTrait(byte)
 	 * @see InkObjectState#getBehavior()
-	 * @since {@literal ink.core}
+	 * @throws ArrayIndexOutOfBoundsException if the index is invalid.
 	 */
 	public <T extends Trait> T asTrait(byte index);
 
+	/**
+	 * Cast this object as a trait. A trait is a part of an object's personality.
+	 * <br/>
+	 * Example:
+	 * <pre>
+	 *     public static final byte t_reflection = 0;
+	 *     Trait trait = this.asTrait("reflection");
+	 *     InkObject object = trait.getTargetBehavior();
+	 * </pre>
+	 * 
+	 * @param role the role of the trait (as defined in this object's class) to which it should be cast.
+	 * @return this object cast to the required trait, or <code>null</code> if the role wasn't found.
+	 */
 	public <T extends Trait> T asTrait(String role);
 
 	/**
-	 * getInkClass. Should be renamed as getClass (not possible) or getKind or getInkClass ?
+	 * Returns the Ink class of this object.
 	 * 
-	 * @return the class of this object
+	 * @return the Ink class of this object.
 	 */
 	public <M extends InkClass> M getMeta();
 
 	/**
-	 * reify (reflect) and return mirror
+	 * Reifies (reflects) and return this object's mirror.
 	 * 
-	 * @return the mirror meta-object for this object
+	 * @return the mirror meta-object for this object.
 	 */
 	public <M extends Mirror> M reflect();
 }
